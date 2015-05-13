24bbcbddf73ca6bff8ab750dd3f705d9c953181b
jeremi <jeremi@ninespin.zyxel.com>
2015-05-13 12:12:56 -0400

    pattern detector: some methods, comments, and constructor

--- Tracking/src/Shapes/PatternDetector.java
+++ Tracking/src/Shapes/PatternDetector.java
@@ -5,10 +5,43 @@
  */
 package Shapes;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
  *
  * @author jeremi
  */
 public class PatternDetector {
+    private ArrayList<Shape> detectedShapes;
+    private BufferedImage frame;
     
+    
+    public PatternDetector(){}
+    public PatternDetector(BufferedImage _frame){
+        frame = _frame;
+        
+    }
+    
+    public void detectShapes(){
+        
+    }
+    
+    public boolean checkIfTemplateExists(Template _template, int errorMargin){
+        /*
+            checks all already saved templates to see if _template matches at errorMargin % of error margin
+                scale the biggest template of the 2 to the size of the smallest before compare
+            if match, return true, else return false(and possibly write a new template accordingly)
+        */
+        
+        return false;
+    }
+    
+    private Shape makeShape(int x0,int y0,int x1, int y1){
+        Template template = new Template();
+        Shape shape = new Shape();
+        
+        
+        return shape;
+    }
 }
