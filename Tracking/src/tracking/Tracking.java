56e31248c065a9f5dcab27c454d5e83889358d1f
ArnaudPare-Vogt <arnaud.parevogt@gmail.com>
2015-05-12 22:21:14 -0400

    bases images

--- Tracking/src/tracking/Tracking.java
+++ Tracking/src/tracking/Tracking.java
@@ -1,9 +1,10 @@
 package tracking;
 
 import IO.FrameStream;
+import graphics.Filter;
+import graphics.FrameProcessor;
+import java.awt.Color;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -16,9 +17,10 @@ public class Tracking {
      */
     public static void main(String[] args) {
         try {
-            FrameStream fs = new FrameStream("");// <-- le path
-            
-            fs.setOutput(null);
+            FrameStream fs = new FrameStream("C:\\Users\\eloi\\Documents\\ArnaudDossiers\\Prog");// <-- le path
+            Filter f = new Filter(Color.red,10);
+            FrameProcessor fp = new FrameProcessor(f);
+            fs.setOutput(fp);
             fs.start();
         } catch (IOException ex) {
             System.out.println("OOOOPs");
